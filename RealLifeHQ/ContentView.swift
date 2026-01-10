057df5fb7ab654fefed5b0dc4dfd92c60340d90b
Sarah Walker <ssewalker2021@gmail.com>
2026-01-10 15:37:11 -0600

    Initial Commit

---
+++ RealLifeHQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealLifeHQ
+//
+//  Created by Sarah Walker on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealLifeHQ/RealLifeHQApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealLifeHQApp.swift
+//  RealLifeHQ
+//
+//  Created by Sarah Walker on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RealLifeHQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
