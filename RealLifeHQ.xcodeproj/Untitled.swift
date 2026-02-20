23e3eb95dfd288dce0a46e7a2a736a737934b501
ssewalker-afk <ssewalker@yahoo.com>
2026-02-20 16:46:08 -0600

    Updates
    updates

---
+++ RealLifeHQ.xcodeproj/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  
+//
+//  Created by Sarah Walker on 2/15/26.
+//
+
--- RealLifeHQ/SubscriptionView.swift
+++ RealLifeHQ/SubscriptionView.swift
@@ -334,7 +334,7 @@ struct SubscriptionView: View {
             // REQUIRED: Links to Privacy Policy and Terms of Use
             VStack(spacing: 8) {
                 HStack(spacing: 16) {
-                    NavigationLink(destination: PrivacyPolicyView()) {
+                    Link(destination: URL(string: "https://ssewalker-afk.github.io/RealLifeHQ/docsprivacy-policy.html")!) {
                         Text("Privacy Policy")
                             .font(.caption)
                             .underline()
@@ -345,7 +345,7 @@ struct SubscriptionView: View {
                         .font(.caption)
                         .foregroundStyle(.secondary)
                     
-                    NavigationLink(destination: TermsOfServiceView()) {
+                    Link(destination: URL(string: "https://ssewalker-afk.github.io/RealLifeHQ/terms-of-service.html")!) {
                         Text("Terms of Use")
                             .font(.caption)
                             .underline()
